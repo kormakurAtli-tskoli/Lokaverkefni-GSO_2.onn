445760bd4f70f5b8713a2a7f1ef03484ab3d23b5
kormakurun285 <kormakurun285@nemi.tskoli.is>
2017-05-03 15:54:06 +0000

    Töflunni 'utgefandi' bætt við

--- lokaverkefni_skripta.sql
+++ lokaverkefni_skripta.sql
@@ -8,3 +8,14 @@ VALUES
 	("Sóló"),
     ("Hljómsveit"),
     ("Dúó");
+
+CREATE TABLE utgefandi(
+	ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nafn VARCHAR(255)
+);
+INSERT INTO
+	utgefandi(nafn)
+VALUES
+	("Columbia Records"),
+    ("Cleveland International Records"),
+    ("EMI Records");
